628116dd53a3eed2976287202311ae676385519a
skbecker <becker.summer@outlook.com>
2022-11-09 19:38:12 -0500

    week4

---
+++ 02-express-tutorial/09-middleware-use.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const logger = require('./logger')
+const authorize = require('./authorize')
+// req => middleware => res
+
+app.use([logger, authorize])
+// api/home/about/products
+
+app.get('/', (req,res) => {
+    res.send('Home')
+})
+
+app.get('/about', (req,res) => {
+    res.send('About')
+})
+
+app.get('api/products', (req,res) => {
+    res.send('Products')
+})
+
+app.get('api/items', (req,res) => {
+    res.send('Items')
+})
+
+app.listen(5000, ()=> {
+    console.log('Server is listening on port 5000....')
+}) 
